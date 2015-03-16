aa6f96072717de55e3553a362c3c8a184118b0b9
HUANGManutea <manutea.huang@insa-rennes.fr>
2015-03-16 16:37:34 +0100

    affectation constante gere

--- yakaCoder/yakaCompiler/src/main/main/Affectation.java
+++ yakaCoder/yakaCompiler/src/main/main/Affectation.java
@@ -4,10 +4,16 @@ import main.Constantes.*;
 
 public class Affectation {
 	private String currentIdent;
+	private boolean erreurAffect=false;
 	
 	public void setCurrentIdent(String ident){
 		if (Yaka.tabIdent.exists(ident)) {
 			currentIdent = ident;
+			if(Yaka.tabIdent.find(currentIdent) instanceof IdConst){
+				System.out.println("Erreur affectation dans une constante.");
+				erreurAffect = true;
+				Yaka.yvm.erreur();
+			}
 		} else {
 			System.out.println("Ident "+ident+" non déclaré");
 			Yaka.yvm.erreur();
@@ -15,13 +21,14 @@ public class Affectation {
 	}
 	
 	public void doAffect(){
-		IdVar v = (IdVar) Yaka.tabIdent.find(currentIdent);
-		Type t = Yaka.expression.popType();
-		if (v.type != t) {
-			System.out.println("Erreur de type dans l'affectation, ligne " + Yaka.token.beginLine);
-			Yaka.yvm.erreur();
+		if(!erreurAffect){
+			IdVar v = (IdVar) Yaka.tabIdent.find(currentIdent);
+			Type t = Yaka.expression.popType();
+			if (v.type != t) {
+				System.out.println("Erreur de type dans l'affectation, ligne " + Yaka.token.beginLine);
+				Yaka.yvm.erreur();	
+			}
+			Yaka.yvm.istore(v.offset);
 		}
-		
-		Yaka.yvm.istore(v.offset);
 	}
 }
